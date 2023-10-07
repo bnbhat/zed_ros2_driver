08c497a7711a534359fae5eb15987b145540e712
balachandra <bhalu311@gmail.com>
2023-10-07 22:29:44 +0200

    update files to match ament_python structure

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from setuptools import find_packages, setup
+from glob import glob
 
 package_name = 'zed_driver'
 
@@ -10,16 +11,19 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/config', glob('config/**/*', recursive=True)),
+        ('share/' + package_name + '/launch', glob('launch/**/*', recursive=True))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='balachandra',
-    maintainer_email='bhalu311@gmail.com',
-    description='TODO: Package description',
+    maintainer='Balachandra Bhat',
+    maintainer_email='bnbhat311@gmail.com',
+    description='Minimal ROS2 Driver for ZED Camera',
     license='MIT',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'zed_ros2_driver = zed_driver.zed_ros_driver:main',
         ],
     },
 )
